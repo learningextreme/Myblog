2586ce129a1597ee8db396135ec350531de8ea58
learningextreme <learnigextreme@gmail.com>
2022-06-16 16:17:00 +0530

    a

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -11,6 +11,12 @@ var store = [{
         "url": "/beamforming/",
         "teaser": null
       },{
+        "title": "My Second Post",
+        "excerpt":"Working with Pages To better organize all of your pages you can centralize them into a single location similar to posts and collections. Step 1: Start by placing pages (.md or .html files) into a _pages directory. Meaningfully naming files should be the goal. Avoid patterns like /about/index.md as it...","categories": [],
+        "tags": ["bash","linux","command"],
+        "url": "/second/",
+        "teaser": null
+      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"This is my first post!!!  ","categories": [],
         "tags": [],
