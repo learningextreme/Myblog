1955ef15711c679c754ee42a444a0c7a69f0fa4c
learningextreme <learnigextreme@gmail.com>
2022-06-16 14:12:29 +0530

    check

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "My Frist Post",
+        "excerpt":"Working with Pages To better organize all of your pages you can centralize them into a single location similar to posts and collections. Step 1: Start by placing pages (.md or .html files) into a _pages directory. Meaningfully naming files should be the goal. Avoid patterns like /about/index.md as it...","categories": [],
+        "tags": ["bash","linux","command"],
+        "url": "/First-post/",
+        "teaser": null
+      },{
+        "title": "Transmit Beamforming TxBF",
+        "excerpt":"Transmit Beamforming TxBF It is another way of transmitting focused energy preferentially towards a client to have better performance and throughput. Technically it is very effective if the client is not too farther away. A wireless client meant to be mobile but, it is very effective if it is stationed...","categories": [],
+        "tags": [],
+        "url": "/beamforming/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"This is my first post!!!  ","categories": [],
+        "tags": [],
+        "url": "/writing-jekyll-posts/",
+        "teaser": null
+      }]
